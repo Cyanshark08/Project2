62c3cfbd45306acb306dca95512bbf68d439b78b
Andrew Nguyen <andreww.nguyen14@gmail.com>
2025-09-03 11:31:12 -0700

    Rational Class
    - Completed Rational Class
    - Will test functionality soon

--- Project2/Project2/src/Rational.cpp
+++ Project2/Project2/src/Rational.cpp
@@ -4,3 +4,124 @@ void Rational::Clear()
 {
 
 }
+
+Rational::Rational()
+{
+	numerator = 0;
+	denominator = 1;
+	simplify();
+}
+
+Rational::Rational(int32_t newNumerator, int32_t newDenominator)
+{
+	numerator = newNumerator;
+	denominator = newDenominator;
+	simplify();
+}
+
+int32_t Rational::gcd(int32_t x, int32_t y) const
+{
+	if (x % y == 0)
+		return y;
+	else
+		return gcd(y, x % y);
+}
+
+void Rational::simplify()
+{
+	int32_t gcd = Rational::gcd(numerator, denominator);
+
+	// divide numerator and denominator by the gcd
+	numerator /= gcd;
+	denominator /= gcd;
+
+	// move negative to top
+	if (denominator < 0)
+	{
+		numerator *= -1;
+		denominator *= -1;
+	}
+}
+
+void Rational::setNumerator(int32_t newNumerator)
+{
+	numerator = newNumerator;
+	simplify();
+}
+
+int Rational::getNumerator() const
+{
+	return numerator;
+}
+
+void Rational::setDenominator(int32_t newDenominator)
+{
+	denominator = newDenominator;
+	simplify();
+}
+
+int Rational::getDenominator() const
+{
+	return denominator;
+}
+
+Rational Rational::operator *(const Rational &right)
+{
+	Rational temp;
+
+	// multiply the numerator and denominator of each object
+	temp.setNumerator(this->getNumerator() * right.getNumerator());
+	temp.setDenominator(this->getDenominator() * right.getDenominator());
+
+	return temp;
+}
+
+
+Rational Rational::operator /(const Rational &right)
+{
+	Rational temp;
+
+	// multiply by the reciprocal
+	temp.setNumerator(this->getNumerator() * right.getDenominator());
+	temp.setDenominator(this->getDenominator() * right.getNumerator());
+
+	return temp;
+}
+
+Rational Rational::operator +(const Rational &right)
+{
+	Rational temp;
+
+	// multiply the numerator by the denominator, then add the numerators
+	temp.setNumerator((right.getDenominator() * this->getNumerator()) + (this->getDenominator() * right.getNumerator()));
+	temp.setDenominator(this->getDenominator() * right.getDenominator());
+
+	return temp;
+}
+
+Rational Rational::operator -(const Rational &right)
+{
+	Rational temp;
+
+	// multiply the numerator by the denominator, then subtract the numerators
+	temp.setNumerator((right.getDenominator() * this->getNumerator()) - (this->getDenominator() * right.getNumerator()));
+	temp.setDenominator(this->getDenominator() * right.getDenominator());
+
+	return temp;
+}
+
+bool Rational::operator ==(const Rational &right)
+{
+	return ((this->getNumerator() * right.getDenominator()) == (this->getDenominator() * right.getNumerator()));
+}
+
+bool Rational::operator <(const Rational &right)
+{
+	return ((this->getNumerator() * right.getDenominator()) < (this->getDenominator() * right.getNumerator()));
+}
+
+std::ostream &operator <<(std::ostream &out, const Rational &obj)
+{
+	out << obj.getNumerator() << "/" << obj.getDenominator();
+	return out;
+}
--- Project2/Project2/src/Rational.h
+++ Project2/Project2/src/Rational.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "ExceptionInterface.h"
+#include <iostream>
 
 class Rational
 {
@@ -14,7 +15,82 @@ public:
 
 	};
 
+	/*
+	* PreCondition: N/A
+	* 
+	* PostCondition: default the numerator and denominator
+	*/
+	Rational();
+
+	/*
+	* PreCondition: newNumerator and newDenominator must be integers. NewDenominator cannot equal 0
+	* 
+	* PostCondition: set newNumerator and newDenominator to numerator and denominator, respectively
+	* @param The integers to set as the numerator and denominator
+	*/
+	Rational(int newNumerator, int newDenominator);
+
+	/*
+	* PreCondition: x and y are intergers. y cannot equal 0.
+	*
+	* PostCondition: return the greatest common divisor between the two integers
+	* @param The two integers to find the greatest common divisor
+	* @return An integer representing the greatest common divisor
+	*/
+	int32_t gcd(int32_t x, int32_t y) const;
+
+	/*
+	* PreCondition: N/A
+	* 
+	* PostCondition: simplify the numerator and denominator
+	*/
+	void simplify();
+
+	/*
+	* PreCondition: newNumerator must be an integer
+	* 
+	* PostCondition: set the numerator to the new numerator
+	* @param The integer to set as the numerator
+	*/
+	void setNumerator(int32_t newNumerator);
+
+	/*
+	* PreCondition: newDenominator must be an integer not equal to 0
+	* 
+	* PostCondition: set the denominator to the new denominator
+	* @param the integer to set as the denominator
+	*/
+	void setDenominator(int32_t newDenominator);
+
+	/*
+	* PreCondition: N/A
+	* 
+	* PostCondition: return the numerator
+	* @return the integer in the numerator
+	*/
+	int32_t getNumerator() const;
+
+	/*
+	* PreCondition: N/A
+	* 
+	* PostCondition: return the denominator
+	* @return the integer in the denominator
+	*/
+	int32_t getDenominator() const;
+
+	// OVERLOADED OPERATORS
+	Rational operator *(const Rational &right);
+	Rational operator /(const Rational &right);
+	Rational operator +(const Rational &left);
+	Rational operator -(const Rational &left);
+	bool operator ==(const Rational &right);
+	bool operator <(const Rational &right);
+	friend std::ostream &operator <<(std::ostream &out, const Rational &obj);
+
+
 private:
+	int32_t numerator;
+	int32_t denominator;
 
 };
 
